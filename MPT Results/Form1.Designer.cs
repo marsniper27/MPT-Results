d75ebff9afa812743f6b5aafac7777a4eed5e656
Kyle Marshall <kyle_marshall_sniper_27@hotmail.com>
2018-01-18 13:09:01 -0400

    finished

--- MPT Results/Form1.Designer.cs
+++ MPT Results/Form1.Designer.cs
@@ -28,33 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.Run = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.checkBox1);
             this.panel1.Controls.Add(this.Run);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(422, 261);
+            this.panel1.Size = new System.Drawing.Size(408, 229);
             this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(123, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 8;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(123, 94);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(158, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Open output once complete";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Run
             // 
@@ -101,6 +139,13 @@
             this.textBox2.Size = new System.Drawing.Size(350, 20);
             this.textBox2.TabIndex = 2;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(350, 20);
+            this.textBox1.TabIndex = 0;
+            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(368, 29);
@@ -111,33 +156,150 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // tabControl1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 29);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(350, 20);
-            this.textBox1.TabIndex = 0;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(422, 261);
+            this.tabControl1.TabIndex = 1;
             // 
-            // checkBox1
+            // tabPage1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(123, 94);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(158, 17);
-            this.checkBox1.TabIndex = 7;
-            this.checkBox1.Text = "Open output once complete";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(414, 235);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Single File";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.panel2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(414, 235);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Multi-file";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(408, 229);
+            this.panel2.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(368, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(28, 19);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Input";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Output";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(18, 29);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(330, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(18, 73);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(330, 20);
+            this.textBox4.TabIndex = 13;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(368, 73);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(28, 19);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(147, 125);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Run";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(166, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(147, 193);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 17;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(422, 261);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "MPT Results";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -153,6 +315,20 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- MPT Results/Form1.cs
+++ MPT Results/Form1.cs
@@ -14,9 +14,11 @@ namespace MPT_Results
 {
     public partial class Form1 : Form
     {
-        public List<Result> results;
+        public List<Result> results = new List<Result>();
         public List<Result> failedTest = new List<Result>();
-        public List<Result> passedTest = new List<Result>(); 
+        public List<Result> passedTest = new List<Result>();
+        public string fileName;
+        public string filePath;
 
         public Form1()
         {
@@ -25,6 +27,7 @@ namespace MPT_Results
 
         private void button1_Click(object sender, EventArgs e)
         {
+            label3.Text = "";
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
 
 
@@ -62,12 +65,16 @@ namespace MPT_Results
             {
 
                 textBox1.Text = openFileDialog1.FileName;
+                fileName = Path.GetFileNameWithoutExtension(textBox1.Text);
+                filePath = Path.GetDirectoryName(textBox1.Text);
+                
 
             }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            label3.Text = "";
             FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
 
             if (folderBrowser.ShowDialog() == DialogResult.OK)
@@ -81,15 +88,19 @@ namespace MPT_Results
 
         private void Run_Click(object sender, EventArgs e)
         {
+            label3.Text = "Reading file";
+
             using (StreamReader r = File.OpenText(textBox1.Text))
             {
                 string json = r.ReadToEnd();
                 results = JsonConvert.DeserializeObject<List<Result>>(json);
             }
 
+
+            label3.Text = "Varifying results";
             foreach (Result x in results)
             {
-                if(x.TotalExpectedWin == x.GameTotalWin)
+                if(x.TotalExpectedWin != x.GameTotalWin)
                 {
                     failedTest.Add(x);
                 }
@@ -99,14 +110,19 @@ namespace MPT_Results
                 }
             }
 
+
+            label3.Text = "Writing output";
             try
             {
-                if(File.Exists(textBox2.Text + "Output.txt"))
+                if(File.Exists(filePath + fileName + "_Results.txt"))
                 {
+                    label3.Text = "";
                     MessageBox.Show("Output folder already contains a results file");
+                    results.Clear();
+                    fileName = "";
                     return;
                 }
-                using (StreamWriter sw = File.CreateText(textBox2.Text + "\\Output.txt"))
+                using (StreamWriter sw = File.CreateText(textBox2.Text + @"\Output.txt"))
                 {
                     if (passedTest.Count() != 0)
                     {
@@ -145,11 +161,152 @@ namespace MPT_Results
             {
                 MessageBox.Show(EX.ToString());
             }
-
+            label3.Text = "COMPLETE";
             if (checkBox1.Checked == true)
             {
-                File.OpenText(textBox2.Text + "\\Output.txt");
+                System.Diagnostics.Process.Start(filePath + fileName + "_Results.txt");
             }
+            results.Clear();
+            fileName = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            label6.Text = "";
+            label7.Text = "";
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                textBox3.Text = folderBrowser.SelectedPath;
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            label6.Text = "";
+            label7.Text = "";
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+
+            {
+
+                textBox4.Text = folderBrowser.SelectedPath;
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            bool complete = false;
+            int count = 0;
+            List <string> fileEntries = new List<string> (Directory.GetFiles(textBox3.Text));
+            while (!complete)
+            {
+                foreach (string filePath in fileEntries)
+                {
+                    if (Path.GetExtension(filePath) != ".testResults")
+                    {
+                        fileEntries.RemoveAt(count);
+                        count= 0;
+                        break;
+                    }
+                    count++;
+                }
+                if(count >= fileEntries.Count())
+                { complete = true; }
+            }
+
+            var numFiles = fileEntries.Count();
+            count = 1;
+            foreach (string filePath in fileEntries)
+            {
+                failedTest.Clear();
+                passedTest.Clear();
+                results.Clear();
+                fileName = Path.GetFileNameWithoutExtension(filePath);
+                
+
+                label6.Text = "Reading file";
+                label7.Text = "File" + count + "of" + numFiles;
+
+                using (StreamReader r = File.OpenText(filePath))
+                {
+                    string json = r.ReadToEnd();
+                    results = JsonConvert.DeserializeObject<List<Result>>(json);
+                }
+
+
+                label3.Text = "Varifying results";
+                foreach (Result x in results)
+                {
+                    if (x.TotalExpectedWin != x.GameTotalWin)
+                    {
+                        failedTest.Add(x);
+                    }
+                    else if (x.TotalExpectedWin == x.GameTotalWin)
+                    {
+                        passedTest.Add(x);
+                    }
+                }
+
+
+                label3.Text = "Writing output";
+                try
+                {
+                    if (File.Exists(textBox4.Text+@"\" + fileName + "_Results.txt"))
+                    {
+                        label3.Text = "";
+                        MessageBox.Show("Output folder already contains a results file");
+                        results.Clear();
+                    }
+                    using (StreamWriter sw = File.CreateText(textBox4.Text + @"\" + fileName + "_Results.txt"))
+                    {
+                        if (passedTest.Count() != 0)
+                        {
+                            if (passedTest.Count() == results.Count())
+                            {
+                                sw.WriteLine("All Tests Passed!");
+                            }
+                            else
+                            {
+                                sw.WriteLine("----------Passed Tests----------");
+                                sw.WriteLine();
+                                sw.WriteLine("Number of Passed Tests: " + passedTest.Count());
+
+                                foreach (Result pass in passedTest)
+                                {
+                                    sw.WriteLine(pass.GamePlayName);
+                                }
+                            }
+                        }
+                        if (failedTest.Count() != 0)
+                        {
+                            sw.WriteLine();
+                            sw.WriteLine("----------Failed Tests----------");
+                            sw.WriteLine();
+                            sw.WriteLine("Number of Failed Tests: " + failedTest.Count());
+                            sw.WriteLine();
+                            foreach (Result fail in failedTest)
+                            {
+                                sw.WriteLine(fail.GamePlayName);
+                            }
+                        }
+                    }
+                }
+                catch (Exception EX)
+                {
+                    MessageBox.Show(EX.ToString());
+                }
+                count++;
+                results.Clear();
+            }
+            label6.Text = "COMPLETE";
+            results.Clear();
         }
     }
 }
